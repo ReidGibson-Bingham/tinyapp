fad5a1c5ad0834f802fd6140b1878dcdb0bcf33a
P4ulH1ll <jsr3b0rn@gmail.com>
2022-02-26 02:27:13 +0000

    slowly starting to understand how ejs and express work together. instructor Brian Sohn advised me to read over the express documentation, it was great advice and i don't know why I didn't think of it sooner...

--- express_server.js
+++ express_server.js
@@ -22,14 +22,30 @@ function getUserWithEmail(email, usersDatabase) {
   return null;
 }
 
+//let loggedIn = true; // a variable to check whether or not a user has already logged in. With this variable, we can allow only users who have logged in to edit and add new links
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+// const urlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// };
+
+
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+  b6UTxQ: {
+      longURL: "https://www.tsn.ca",
+      userID: "aJ48lW"
+  },
+  i3BoGr: {
+      longURL: "https://www.google.ca",
+      userID: "aJ48lW"
+  }
+}; 
+
+// when refactoring these keys into objects containing the long url and the user_id, i'm not too sure how to access the correct keys. ie urlDatabase[somevariable].longUrl
 
 const users = { 
   "userRandomID": {
@@ -45,32 +61,43 @@ const users = {
 }
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  //let objId = req.params;
+  // const longURL = urlDatabase[objId].shortURL; // I think this is what i have to do in compass' second last activity
+  //const longURL = urlDatabase[req.params.shortURL][longURL];
+  console.log("req.params: ", req.params);
+  res.redirect(urlDatabase[req.params.shortURL].longURL);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+    const userId = req.cookies.user_id;
+    const user = users[userId];
+    const templateVars = {user: user};
+    res.render("urls_new", templateVars, ); 
+  
 });
 
+//the /urls page should not display URLs unless the user is logged in
 app.get("/urls", (req, res) => {
 
   const userId = req.cookies.user_id;
-  console.log("userID: ", userId);
+  //console.log("req.params: ", req.params);
   const user = users[userId];
-  console.log("user: ", user);
-  const templateVars = { urls: urlDatabase, user: user};
-  // const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
-  console.log("user.id: ", user);
+  //console.log("user: ", user);
+  //console.log("userId: ", users[userId].id);
+  const templateVars = { urls: urlDatabase, user: user, users: users};
+
   res.render("urls_index", templateVars);
+  
 });
 
 
 app.get("/urls/:shortURL", (req, res) => {
 
+  const userId = req.cookies.user_id;
+  const user = users[userId];
   
-  
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  // vv this is where i start to get confused about req.params etc
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL, user: user};
   res.render("urls_show", templateVars);
 });
 
@@ -92,22 +119,31 @@ app.get("/register", (req, res) => {
 
 app.get("/login", (req, res) => {
   //console.log("req.body: ", req.body);
-  const templateVars = {user: null};
+  const userId = req.cookies.user_id;
+  const user = users[userId];
+  console.log("test:", userId);
+  const templateVars = {user: user};
   res.render("login", templateVars);
+  
 })
 
 
 app.post("/urls/:shortURL", (req, res) => {
   //console.log(req.body);  // Log the POST request body to the console
   //res.send("testing");         // Respond with 'Ok' (we will replace this)
-  urlDatabase[req.params.shortURL] = req.body.longURL;
+  //urlDatabase[req.params.shortURL].longURL = req.body.longURL;
+  //console.log("user-input :")
+  const newLongURL = req.body.updateURL;
+  urlDatabase[req.params.shortURL].longURL = newLongURL;
+  console.log("newURL: ", newLongURL);
   res.redirect("/urls");
 });
 
 
 // vv this POST route will remove a URL
 app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
+  delete urlDatabase[req.params.shortURL].longURL;
+  
   res.redirect("/urls");
 });
 
@@ -117,21 +153,25 @@ app.post("/login", (req,res) => {
   // res.cookie("username", req.body["username"]); // object username
   //console.log(req);
   res.cookie("password", req.body["password"]);
-  let userObject = req.body;
-  console.log("user object:", userObject);
-  const user = getUserWithEmail(userObject.email, users);
-  if (!user) {
+  const testEmail = req.body.email;
+  const testPassword = req.body.password;
+  // let userObject = req.body;
+  // console.log("user object:", userObject);
+  const checkUser = getUserWithEmail(testEmail, users);
+  if (!checkUser) {
     res.status(403);
     res.send('Email not found');
+    loggedIn = false;
     return;
   } else {
-    if (userObject.password !== user.password) {
+    if (testPassword !== checkUser.password) {
       res.status(403);
       return res.send('email password combination does not exist');
     }
+    loggedIn = false;
   }
   
-  res.cookie("user_id", user.id);
+  res.cookie("user_id", checkUser.id);
   res.redirect("/urls");
 
 });
@@ -139,7 +179,8 @@ app.post("/login", (req,res) => {
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
   res.redirect("/urls");
-  //console.log(users);
+  //setTimeout(() => {res.redirect("/urls")}, 3000);// redirecting the user to the homepage after seeing an email not found message if they've provided incorrect info
+  
 });
 
 
